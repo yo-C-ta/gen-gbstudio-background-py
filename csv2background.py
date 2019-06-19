941c324013662ea82a75cf7aae91ef4c4b631519
yo-C-ta <45458949+yo-C-ta@users.noreply.github.com>
2019-06-20 01:06:30 +0900

    Initial commit

---
+++ csv2background.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+
+import sys
+import json
+import csv
+import argparse
+from PIL import Image
+
+
+from pprint import pprint
+
+GRID_PIX = 8
+
+
+def genbg(config, design, outpng):
+    field = Image.new(
+        "RGBA",
+        (config["x"] + config["width"], config["y"] + config["height"]),
+        config["basecolor"],
+    )
+
+    for key, value in config["layer"].items():
+        if not value:
+            continue
+
+        if value.startswith("#"):
+            size = (GRID_PIX, GRID_PIX)
+            patch = Image.new("RGBA", size, value)
+        else:
+            patch = Image.open(value)
+            size = (patch.width, patch.height)
+
+        for y, row in enumerate(design):
+            for x, cell in enumerate(row):
+                if cell == key:
+                    region = patch.crop(box=(0, 0, size[0], size[1]))
+                    field.paste(
+                        region,
+                        box=(
+                            x * GRID_PIX,
+                            y * GRID_PIX,
+                            x * GRID_PIX + size[0],
+                            y * GRID_PIX + size[1],
+                        ),
+                        mask=patch,
+                    )
+    field.crop(
+        box=(
+            config["x"],
+            config["y"],
+            config["x"] + config["width"],
+            config["y"] + config["height"],
+        )
+    ).save(outpng)
+
+
+if __name__ == "__main__":
+    ARGPARSE = argparse.ArgumentParser(description="Generate GB background image.")
+    ARGPARSE.add_argument(
+        "csv", type=str, default=None, help="Path to field design file."
+    )
+    ARGPARSE.add_argument(
+        "json", type=str, default=None, help="Path to configuration file."
+    )
+    ARGPARSE.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="background.png",
+        help="Output png filename.",
+    )
+    ARGS = ARGPARSE.parse_args()
+
+    with open(ARGS.json, "r") as jf:
+        jsonData = json.load(jf)
+
+    with open(ARGS.csv, "r") as cf:
+        csvData = [row for row in csv.reader(cf) if row]
+
+    sys.exit(
+        genbg(jsonData, csvData, ARGS.output)
+    ) if jsonData and csvData else sys.exit(1)
